63dfd5b9c31544e9d9e615d5191cacfcc6956e88
rururux <rururux@gmail.com>
2022-03-29 18:01:31 +0900

    画像サイズを制限

--- src/DogImage.js
+++ src/DogImage.js
@@ -1,10 +1,11 @@
 // DO NOT DELETE
 import * as React from "react"
+import "../styles/dogImage.css"
 
 // eslint-disable-next-line
 const DogImage = ({ src }) => {
   return (
-    <img src={src} alt="かわいい犬の画像" />
+    <img className="dog-image" src={src} alt="かわいい犬の画像" />
   )
 }
 
